2f870bea77f8ae6a6095627eb4e1542c2b49c2b4
DevifyPro <149935052+DevifyPro@users.noreply.github.com>
2024-06-05 14:05:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+{
+  "version": 1,
+  "defaultLocale": {
+    "packageLocale": "en-US",
+    "publisher": "DevifyPro",
+    "name": "pcgenapps",
+    "shortDescription": "Repository for PCGen apps",
+    "description": "A collection of software packages for PCGen",
+    "tags": [
+      "pcgen",
+      "apps"
+    ]
+  },
+  "packages": [teamviewer]
+}
 
